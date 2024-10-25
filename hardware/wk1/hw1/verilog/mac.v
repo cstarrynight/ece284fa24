62d540c0cef30520e636b10de3bf9e8da7799722
Christina <ctmai@ucsd.edu>
2024-10-24 18:33:20 -0700

    Start HW3

--- hardware/wk1/hw1/verilog/mac.v
+++ hardware/wk1/hw1/verilog/mac.v
@@ -1,3 +1,4 @@
+// Code your design here
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
 module mac (out, A, B, format, acc, clk, reset);
@@ -22,6 +23,27 @@ reg signed [bw-1:0] b_q;
 assign out = psum_q;
 
 // Your code goes here
+  
 
+  
+  always @ (posedge clk or posedge reset) begin
+	a_q = A;
+    b_q = B;
+    if (reset) begin
+        psum_q <= 0; 
+    end
+  	if(acc) begin
+        
+        if(format) begin //mac in a sign and magnitude system
+          
+          psum_q <= psum_q + {(a_q[bw-1] ^ b_q[bw-1]) ? 1'b1 : 1'b0, a_q[bw-2:0] * b_q[bw-2:0]};
+        end
 
+        else begin //mac operation in a signed 2’s complement number system
+          psum_q <= psum_q + a_q * b_q;    
+        end
+
+    end
+end
 endmodule
+
