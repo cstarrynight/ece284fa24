df8a6f31780ce2ac1e68e2e0e1d9ec685dfdadd0
Christina <ctmai@ucsd.edu>
2024-10-30 21:26:11 -0700

    d

--- hardware/wk3/prob3/verilog/adder.v
+++ hardware/wk3/prob3/verilog/adder.v
@@ -6,10 +6,10 @@ module adder (out, a, b);
 parameter psum_bw = 16;
 
 //...
-input  [psum_bw-1:0] a; //activation
-input  [psum_bw-1:0] b; //weight, not bias
+input  [psum_bw/2-1:0] a; //activation
+input  [psum_bw/2-1:0] b; //weight, not bias
 
-output [psum_bw/2+1:0] out;
+output [psum_bw/2-1:0] out;
     
 assign out = a+b;
 
