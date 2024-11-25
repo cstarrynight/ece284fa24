bbd460bc5fac9a2ad9ded225331e5185260de902
Christina <ctmai@ucsd.edu>
2024-11-25 10:53:09 -0800

    s

--- hardware/wk5/hw1_2/verilog/mac_row.v
+++ hardware/wk5/hw1_2/verilog/mac_row.v
@@ -1,5 +1,7 @@
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
+`include "mac_tile.v"
+
 module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
 
   parameter bw = 4;
@@ -7,13 +9,15 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
   parameter col = 8;
 
   input  clk, reset;
-  output [psum_bw*col-1:0] out_s;
+  output [psum_bw*col-1:0] out_s; //16*8-1:0
   output [col-1:0] valid;
   input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
   input  [1:0] inst_w;
-  input  [psum_bw*col-1:0] in_n;
+  input  [psum_bw*col-1:0] in_n; //16*8-1:0
 
-  wire  [(col+1)*bw-1:0] temp;
+  wire  [(col+1)*bw-1:0] temp; //9*4-1:0
+
+  wire  [1:0] temp_inst; 
 
   assign temp[bw-1:0]   = in_w;
 
@@ -24,10 +28,10 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
          .reset(reset),
 	 .in_w( temp[bw*i-1:bw*(i-1)]),
 	 .out_e(temp[bw*(i+1)-1:bw*i]),
-	 .inst_w(...),
-	 .inst_e(...),
-	 .in_n(...),
-	 .out_s(...));
+	 .inst_w(temp_inst),
+	 .inst_e(temp_inst),
+	 .in_n(in_n[psum_bw*i-1:psum_bw*(i-1)]),
+	 .out_s(out_s[psum_bw*i-1:psum_bw*(i-1)]));
   end
 
 endmodule
