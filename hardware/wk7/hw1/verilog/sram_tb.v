095f82f72566e6ef0864afc366668d848d66fa22
Christina <ctmai@ucsd.edu>
2024-11-27 18:01:28 -0800

    fix error

--- hardware/wk7/hw1/verilog/sram_tb.v
+++ hardware/wk7/hw1/verilog/sram_tb.v
@@ -38,7 +38,7 @@ initial begin
   $dumpfile("sram_tb.vcd");
   $dumpvars(0,sram_tb);
 
-  x_file = $fopen("activation.txt", "r");
+  x_file = $fopen("my_activation.txt", "r");
 
   // Following three lines are to remove the first three comment lines of the file
   x_scan_file = $fscanf(x_file,"%s", captured_data);
